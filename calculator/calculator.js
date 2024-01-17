40db565d51c1f4cbf40755ab5149b8ce263f304f
Salah Eddin <salah.extras@gmail.com>
2024-01-17 12:55:03 +0100

    Añadido botone clear

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -2,26 +2,52 @@ class Calculator {
     constructor(operand1Element, operand2Element) {
     this.operand1Element = operand1Element;
     this.operand2Element = operand2Element;
-    this.clear();
+    // this.clear();
     }
 
     clear() {
         this.operand1 = 0;
         this.operand2 = 0;
         this.operator = '';
+        this.updateUI();
+        console.log("hi");
+    }
 
-        this.operand1Element.innerHTML = 5;
-        this.operand2Element.innerHTML = 3;
+    updateUI (){
+        this.operand1Element.innerHTML = this.operand1 + this.operator;
+        this.operand2Element.innerHTML = this.operand2;
+    }
 
-        // console.log("testeo");
-        // this.operand1Element.innerHTML = 4;//this.operand1.toString + this.operator;
-        // this.operand2Element.innerHTML = 3; //this.operand2.toString;
+    appendNumber(number) {
+        // if (this.operand2.toString === '0') this.operand2 = +number;
+        // this.operand2 += number.toString;
+        console.log('fjdlsjfksld');
+        this.operand2 += number;
+        this.updateUI();
     }
 }
 
+
+
+
+// HTML buttons catching
 const operand1Element = document.querySelector("[data-operand-1]");
 const operand2Element = document.querySelector("[data-operand-2]");
+const clearButton = document.querySelector("[data-clear]");
+const numbersButtons = document.querySelectorAll("[data-number]");
 
 const myCalculator = new Calculator(operand1Element, operand2Element);
+
+// calculator buttons events
+clearButton.addEventListener("click", () => {
+    myCalculator.clear();
+});
+
+
+numbersButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        myCalculator.appendNumber(button.innerHTML);
+    });
+});
 
 
